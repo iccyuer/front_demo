9fd67fd783aa32247ac8c4f195b826c2cc8e4502
yankuan <yankuan@kongzhong.com>
2022-03-30 15:55:29 +0800

    update

---
+++ src/js/module.js
@@ -0,0 +1,8 @@
+
+export let data = {
+  name: 'haa'
+}
+// Uncaught SyntaxError: The requested module './js/module.js' does not provide an export named 'default'
+export default {
+  name: 'dasdas'
+}
